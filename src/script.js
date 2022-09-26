3e20c29bcc14a968ff5467ed8cdeacedbccac78c
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-09-26 12:43:01 +0700

    eduvation section done

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+//Navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+
 const btnBurger = document.querySelector("#btn-hamburger");
 const nav = document.querySelector("#nav-menu")
 
